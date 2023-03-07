73790168e83197089e742fa7ae7e543aa754ef7a
Jesse <jesse.j.lamsa@gmail.com>
2023-03-07 14:59:31 +0200

    STYLET JÄI NYT KESKEN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from 'styled-components';
+import '.styles/main.css'
 
-function App() {
+import { getFirestore, getDoc, doc } from 'firebase/firestore/lite';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+function App({ app }) {
+
+  const [levelImage, setLevelImage] = React.useState()
+
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+
+  React.useEffect(()=> {
+    getLevelCoordinates()
+    getLevelPhoto();
+  }, [])
+
+  async function getLevelCoordinates(level = 1) {
+    const coordsRef = doc(db, `coordinates/level${level}`);
+    const coordsSnap = await getDoc(coordsRef)
+    console.log(coordsSnap.data())
+
+  }
+
+  async function getLevelPhoto(level = 1) {
+    const levelImageUrl = await getDownloadURL(ref(storage, `level-images/level${level}.jpeg`));
+    setLevelImage(levelImageUrl)
+    console.log(levelImageUrl)
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <StyledImage src={levelImage} alt="level-image" />
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+
+`
+
+const StyledImage = styled.img`
+  height: 100vh;
+`
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA89RPljda5e4CZ9GgusetwyQSSM83XWks",
+    authDomain: "photo-tagging-app-6ab0d.firebaseapp.com",
+    projectId: "photo-tagging-app-6ab0d",
+    storageBucket: "photo-tagging-app-6ab0d.appspot.com",
+    messagingSenderId: "405619596566",
+    appId: "1:405619596566:web:24ba3474f85eb391310437"
+  };
